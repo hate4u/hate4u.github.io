93673641d3f2904ad1cb7b442bb5fb233af6140d
hate4u <dreamston18@gmail.com>
2019-11-08 15:50:28 +0300

    auth form
    Need rework auth form via sql db

---
+++ user_authentication/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login Form Using jQuery - Demo Preview</title>
+    <meta name="robots" content="noindex, nofollow">
+    <!-- Include CSS File Here -->
+    <link rel="stylesheet" href="style.css" />
+    <!-- Include CSS File Here -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="login.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="main">
+            <form class="form" method="post" action="#">
+                <h2>Create Login Form Using jQuery</h2>
+                <label>Email :</label>
+                <input type="text" name="demail" id="email">
+                <label>Password :</label>
+                <input type="password" name="password" id="password">
+                <input type="button" name="login" id="login" value="Login">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
